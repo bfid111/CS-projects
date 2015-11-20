b0b0fa17c048df70fda3552e2d89723990e70d18
bfid111 <bfid111@gmail.com>
2015-11-19 19:50:28 -0500

    A

---
+++ PEMDAS/Driver.java
@@ -0,0 +1,69 @@
+
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Driver {
+    private static final char add = '+',  sub = '-';
+    private static final char mult = '*', div= '/';
+
+    private Stack stack;
+
+    public Driver() {
+        stack = new Stack();
+    }
+
+    public int evaluate(String expr) {
+   
+
+        stack = new Stack();
+        int pop1,pop2;
+        int result = 0;
+        String token;
+        StringTokenizer tokenizer = new StringTokenizer(expr);
+
+        while (tokenizer.hasMoreTokens()) {
+            token = tokenizer.nextToken();
+
+            if (isOperator(token)) {
+                pop2 = ((Integer) stack.pop());
+                pop1 = ((Integer) stack.pop());
+                result = operators(token.charAt(0), pop1, pop2);
+                stack.push(result);
+            }
+            else
+                stack.push(Integer.parseInt(token));
+        }
+        result = ((Integer) stack.pop());
+        return result;
+    }
+
+
+    private boolean isOperator(String token) {
+        return (
+            token.equals("+")
+                || token.equals("-")
+                || token.equals("*")
+                || token.equals("/"));
+    }
+
+    private int operators(char operation, int op1, int op2) {
+        int result = 0;
+
+        switch (operation) {
+            case add :
+                result = op1 + op2;
+                break;
+            case sub :
+                result = op1 - op2;
+                break;
+            case  mult :
+                result = op1 * op2;
+                break;
+            case div :
+                result = op1 / op2;
+        }
+
+        return result;
+    }
+}
---
+++ PEMDAS/Main.java
@@ -0,0 +1,28 @@
+
+import java.io.*;
+
+public class Main
+{
+   /**
+    *  Reads and evaluates multiple postfix expressions.
+     * @param args
+     * @throws java.io.IOException
+    */
+   public static void main (String[] args) throws IOException
+   {
+          String expression, again;
+           int result;
+            BufferedReader in = new 
+            BufferedReader( new InputStreamReader(System.in));
+            Driver evaluator = new Driver();
+            System.out.println ("Enter a valid postfix expression: ");
+            expression = in.readLine();
+            result = evaluator.evaluate (expression);
+            System.out.println();
+            System.out.println ("Result: " + result);
+            System.out.println();
+            
+   }
+}
+   
+
